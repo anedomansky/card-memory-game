5736b6edf3b2b1514683a2262e15f1577aad490b
anedomansky <anedomansky@googlemail.com>
2021-01-12 19:10:40 +0100

    Initial setup

---
+++ src/main/java/com/jsf/app/Home.java
@@ -0,0 +1,23 @@
+package com.jsf.app;
+
+import javax.faces.bean.ManagedBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ManagedBean(name="home", eager=true)
+public class Home {
+	private List<String> gameModes;
+	
+	public Home() {
+		this.gameModes = new ArrayList<String>();
+		this.gameModes.add("Custom");
+		this.gameModes.add("Small");
+		this.gameModes.add("Medium");
+		this.gameModes.add("Big");
+	}
+	
+	public List<String> getGameModes() {
+		return this.gameModes;
+	}
+}
