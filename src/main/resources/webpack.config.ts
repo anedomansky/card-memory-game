ffaab128bc6e66b0bf1fe93da26b212cb027cf6b
anedomansky <anedomansky@googlemail.com>
2021-01-16 11:11:00 +0100

    Added more webpack configuration. Added ESLint.

--- src/main/resources/webpack.config.ts
+++ src/main/resources/webpack.config.ts
@@ -1,14 +1,45 @@
 import webpack from 'webpack';
 import path from 'path';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import FriendlyErrorsWebpackPlugin from 'friendly-errors-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 const config: webpack.Configuration = {
     module: {
         rules: [
+            {
+                test: /\.s?css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader',
+                options: {
+                    // disable type checker - use it in fork plugin
+                    transpileOnly: true,
+                },
+            },
 
         ],
     },
     plugins: [
-
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+        }),
+        new FriendlyErrorsWebpackPlugin(),
+        new ForkTsCheckerWebpackPlugin({
+            eslint: {
+                files: './ts/**/*.ts',
+            },
+        }),
     ],
     resolve: {
         extensions: ['.ts', '.js'],
