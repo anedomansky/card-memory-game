d5754d868fcdeb5727f459714c0122adce610f35
anedomansky <anedomansky@googlemail.com>
2021-03-06 10:45:45 +0100

    Implemented the card opening toggle.

--- src/main/webapp/resources/ts/game.ts
+++ src/main/webapp/resources/ts/game.ts
@@ -1,1 +1,10 @@
 import '../scss/game.scss';
+
+const toggleClasses = (btnWithImg: Element) => {
+    btnWithImg.classList.toggle('btn--show');
+    btnWithImg.children[0].classList.toggle('img--show');
+};
+
+const buttons = Array.from(document.getElementsByClassName('content__btn'));
+
+buttons.forEach((btn) => btn.addEventListener('click', () => toggleClasses(btn)));
