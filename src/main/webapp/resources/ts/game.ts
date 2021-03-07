ae73ca2e2b500b36a0041a8c4e8e21de264dab76
anedomansky <anedomansky@googlemail.com>
2021-03-07 10:38:32 +0100

    Worked on the game flow logic.

--- src/main/webapp/resources/ts/game.ts
+++ src/main/webapp/resources/ts/game.ts
@@ -1,10 +1,41 @@
 import '../scss/game.scss';
 
-const toggleClasses = (btnWithImg: Element) => {
-    btnWithImg.classList.toggle('btn--show');
-    btnWithImg.children[0].classList.toggle('img--show');
-};
+let shownElements = 0;
+let matches = 0;
 
 const buttons = Array.from(document.getElementsByClassName('content__btn'));
+
+const neededMatches = buttons.length;
+
+const toggleClasses = (btnWithImg: Element) => {
+    btnWithImg.classList.add('btn--show');
+    btnWithImg.children[0].classList.add('img--show');
+
+    shownElements += 1;
+
+    if (shownElements === 2) {
+        const visibleCards = Array.from(document.getElementsByClassName('btn--show'));
+        const firstCard = visibleCards[0].children[0] as HTMLImageElement;
+        const secondCard = visibleCards[1].children[0] as HTMLImageElement;
+        visibleCards.forEach((btn) => {
+            if (firstCard.alt === secondCard.alt) {
+                btn.removeEventListener('click', () => toggleClasses(btn));
+                btn.classList.replace('btn--show', 'match');
+                matches += 1;
+            } else {
+                setTimeout(() => {
+                    btn.classList.remove('btn--show');
+                    btn.children[0].classList.remove('img--show');
+                }, 500);
+            }
+        });
+
+        shownElements = 0;
+    }
+
+    if (matches === neededMatches) {
+        document.getElementsByClassName('win')[0].classList.add('win--show');
+    }
+};
 
 buttons.forEach((btn) => btn.addEventListener('click', () => toggleClasses(btn)));
